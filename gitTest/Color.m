d28a51319b7d9356bc9b2956958b0298a3261fd8
Alejandro Zielinsky <azielinsky95@gmail.com>
2018-05-02 11:44:42 -0400

    colours and manager

---
+++ gitTest/Color.m
@@ -0,0 +1,13 @@
+//
+//  Color.m
+//  gitTest
+//
+//  Created by Alejandro Zielinsky on 2018-05-02.
+//  Copyright © 2018 Alejandro Zielinsky. All rights reserved.
+//
+
+#import "Color.h"
+
+@implementation Color
+
+@end
---
+++ gitTest/ModelManager.h
@@ -0,0 +1,16 @@
+//
+//  ModelManager.h
+//  gitTest
+//
+//  Created by Alejandro Zielinsky on 2018-05-02.
+//  Copyright © 2018 Alejandro Zielinsky. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "Color.h"
+@interface ModelManager : NSObject
+
+@property NSArray<Color*>* colors;
+
+
+@end
